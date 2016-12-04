e1aa26c5cc84dd5fd6f450e10e85308340f3ff57
Yann Vaillant <yann@vaillant.im>
2016-12-04 21:22:16 +0100

    Remove deadcode
    ping @volnt

--- app/mpv.py
+++ app/mpv.py
@@ -7,7 +7,6 @@ import subprocess
 import shlex
 
 
-# Read a message from stdin and decode it.
 def getMessage():
     rawLength = sys.stdin.read(4)
     if len(rawLength) == 0:
@@ -17,24 +16,9 @@ def getMessage():
     return json.loads(message)
 
 
-# Encode a message for transmission,
-# given its content.
-def encodeMessage(messageContent):
-    encodedContent = json.dumps(messageContent)
-    encodedLength = struct.pack('@I', len(encodedContent))
-    return {'length': encodedLength, 'content': encodedContent}
-
-
-# Send an encoded message to stdout
-def sendMessage(encodedMessage):
-    sys.stdout.write(encodedMessage['length'])
-    sys.stdout.write(encodedMessage['content'])
-    sys.stdout.flush()
-
-
 while True:
     mpv_args = getMessage()
-    if (len(mpv_args) > 1):
+    if len(mpv_args) > 1:
         args = shlex.split("mpv " + mpv_args)
         subprocess.call(args)
         sys.exit(0)
